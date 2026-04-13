72b8b9f53bca5e9300ed04eee7960b5f41a24d27
Pavlo <kanivets.pavel@gmail.com>
2026-04-13 19:24:38 +0300

    Solution

---
+++ main.py
@@ -0,0 +1,18 @@
+def format_linter_error(error):
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8",
+        }
+
+def format_single_linter_file(file_path, errors):
+    return {"errors": [format_linter_error(e) for e in errors],
+            "path": file_path,
+            "status": "failed" if errors else "passed",
+            }
+
+def format_linter_report(linter_report):
+    return [format_single_linter_file(path, errors)
+            for path, errors in linter_report.items()]
